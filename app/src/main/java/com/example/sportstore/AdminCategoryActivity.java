8c90fb7cf72c4a3abbaeae0ce35a9e3c5e62db9d
Sumit Jindal <sumitjindal66@gmail.com>
2020-04-06 07:53:40 -0400

    added category activity

---
+++ app/src/main/java/com/example/sportstore/AdminCategoryActivity.java
@@ -0,0 +1,161 @@
+package com.example.sportstore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class AdminCategoryActivity extends AppCompatActivity {
+
+    private ImageView swimming, running, yoga;
+    private ImageView fitness, athletic, boating;
+    private ImageView basketball, football, cricket;
+    private ImageView golf, fishing, tennis;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_category);
+
+        swimming = (ImageView) findViewById(R.id.swimming);
+        running = (ImageView) findViewById(R.id.running);
+        yoga = (ImageView) findViewById(R.id.yoga);
+
+        fitness = (ImageView) findViewById(R.id.fitness);
+        athletic = (ImageView) findViewById(R.id.athletic);
+        boating = (ImageView) findViewById(R.id.boating);
+
+        basketball = (ImageView) findViewById(R.id.basketball);
+        football = (ImageView) findViewById(R.id.football);
+        cricket = (ImageView) findViewById(R.id.cricket);
+
+        golf = (ImageView) findViewById(R.id.golf);
+        fishing = (ImageView) findViewById(R.id.fishing);
+        tennis = (ImageView) findViewById(R.id.tennis);
+
+
+        swimming.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
+                intent.putExtra("category", "swimming");
+                startActivity(intent);
+            }
+        });
+
+        running.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
+                intent.putExtra("category", "running");
+                startActivity(intent);
+            }
+        });
+
+        yoga.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
+                intent.putExtra("category", "yoga");
+                startActivity(intent);
+            }
+        });
+
+        fitness.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
+                intent.putExtra("category", "fitness");
+                startActivity(intent);
+            }
+        });
+
+        athletic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
+                intent.putExtra("category", "athletic");
+                startActivity(intent);
+            }
+        });
+
+        boating.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
+                intent.putExtra("category", "boating");
+                startActivity(intent);
+            }
+        });
+
+        basketball.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
+                intent.putExtra("category", "basketball");
+                startActivity(intent);
+            }
+        });
+
+        football.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
+                intent.putExtra("category", "football");
+                startActivity(intent);
+            }
+        });
+
+        cricket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
+                intent.putExtra("category", "cricket");
+                startActivity(intent);
+            }
+        });
+
+        golf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
+                intent.putExtra("category", "golf");
+                startActivity(intent);
+            }
+        });
+
+        fishing.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
+                intent.putExtra("category", "fishing");
+                startActivity(intent);
+            }
+        });
+
+        tennis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
+                intent.putExtra("category", "tennis");
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
